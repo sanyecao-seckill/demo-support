9d4028dbe8374fea36668e3a3b47a4e9860aa973
wangzhangfei5 <wangzhangfei5@jd.com>
2021-09-09 21:14:10 +0800

    新增根据订单号查询订单金额

--- demo-support-launcher/src/main/java/com/demo/support/export/impl/SettlementExportServiceImpl.java
+++ demo-support-launcher/src/main/java/com/demo/support/export/impl/SettlementExportServiceImpl.java
@@ -2,11 +2,13 @@ package com.demo.support.export.impl;
 
 import com.demo.support.ActivityService;
 import com.demo.support.SettlementService;
+import com.demo.support.dao.OrderRecord;
 import com.demo.support.dto.Result;
 import com.demo.support.dto.SettlementDataDTO;
 import com.demo.support.dto.SettlementDataRequestDTO;
 import com.demo.support.dto.SettlementOrderDTO;
 import com.demo.support.export.SettlementExportService;
+import com.demo.support.mapper.OrderRecordMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,9 @@ public class SettlementExportServiceImpl implements SettlementExportService {
 
     @Autowired
     SettlementService settlementService;
+
+    @Autowired
+    OrderRecordMapper orderRecordMapper;
 
     @Override
     public Result<SettlementDataDTO> settlementData(SettlementDataRequestDTO requestDTO) {
@@ -37,7 +42,8 @@ public class SettlementExportServiceImpl implements SettlementExportService {
 
     @Override
     public Result<String> getPayPageUrl(String orderId) {
-        String payPageUrl = "http://localhost:8080/mock/payPage?orderId="+orderId+"&orderPrice=1998";
+        OrderRecord orderRecord = orderRecordMapper.selectByOrderId(orderId);
+        String payPageUrl = "http://localhost:8080/mock/payPage?orderId="+orderId+"&orderPrice="+orderRecord.getOrderPrice().toPlainString();
         return new Result<>(payPageUrl);
     }
 }
